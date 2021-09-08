a63f531122616a0fde24f7f4be1d990e3dbfe138
Sam McCord <sam.mccord@protonmail.com>
2021-09-08 13:37:28 -0600

    test esmodule publishing

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -1,4 +1,4 @@
-import { World, getComponent, Component, ID, hasComponent, getID } from '../src'
+import { World, getComponent, Component, ID, hasComponent, getID } from './index'
 
 test('basic ecs functionality works', () => {
   const world = new World()
